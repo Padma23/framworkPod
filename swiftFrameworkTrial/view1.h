00b9417fdbae3b18142f6007d6f6f7749ed77917
Padma Sravani Bhavanasi <padma-2905@padma-2905.csez.zohocorpin.com>
2017-06-02 11:15:09 +0530

    checking

--- swiftFrameworkTrial/view1.h
+++ swiftFrameworkTrial/view1.h
@@ -1,14 +1,13 @@
 //
-//  swiftClass.swift
+//  view1.h
 //  swiftFrameworkTrial
 //
 //  Created by Padma Sravani Bhavanasi on 01/06/17.
 //  Copyright © 2017 Padma Sravani Bhavanasi. All rights reserved.
 //
 
-import Foundation
-@objc public class trialClass: NSObject {
-    static func getTheValue() -> Int{
-        return 5;
-    }
-}
+#import <UIKit/UIKit.h>
+
+@interface view1 : UIView
+
+@end
---
+++ swiftFrameworkTrial/view1.m
@@ -0,0 +1,21 @@
+//
+//  view1.m
+//  swiftFrameworkTrial
+//
+//  Created by Padma Sravani Bhavanasi on 01/06/17.
+//  Copyright © 2017 Padma Sravani Bhavanasi. All rights reserved.
+//
+
+#import "view1.h"
+
+@implementation view1
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
